05f55833da195674ba9772885a6dfa46dbb090d7
Avieral <78261511+Avieral@users.noreply.github.com>
2022-12-24 08:55:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import login from './components/login';
+import signin from './components/signin';
+
+function App() {
+  return (
+    <div className="App">
+      <signin />
+      <login />
+      
+    </div>
+  );
+}
+
+export default App;
